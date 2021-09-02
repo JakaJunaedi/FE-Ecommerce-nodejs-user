6f4709969b790f1b206ccab7efab140287f5a49c
jaka junaedi <jaka.junaedi03@gmail.com>
2021-09-02 14:33:23 +0700

    config url client home heroku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -3,7 +3,8 @@ export const baseUrl = "https://nodejs-ecommerc.herokuapp.com"; //Base Backend H
 //export const urlResetPassword = "http://localhost:2000/api/admin/reset-password" // reset password local
 export const urlResetPassword = "https://nodejs-ecommerc.herokuapp.com/api/admin/reset-password"
 
-export const baseHome = "http://localhost:3000" //Home Base client
+//export const baseHome = "http://localhost:3000" //Home Base client
+export const baseHome = "https://nodejs-ecommerce-user.herokuapp.com" // Home client Heroku
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
