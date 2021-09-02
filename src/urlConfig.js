2be9274e104785938398a86749d1325e15fa46dd
jaka junaedi <jaka.junaedi03@gmail.com>
2021-09-02 14:20:33 +0700

    set urlConfig hreoku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,9 @@
-const baseUrl = "https://nodejs-ecommerc.herokuapp.com"; //Base Backend Heroku
-//const baseUrl = "http://localhost:2000"; //Base Backend local
+//export const baseUrl = "https://nodejs-ecommerc.herokuapp.com"; //Base Backend Heroku
+export const baseUrl = "http://localhost:2000"; //Base Backend local
+//export const urlResetPassword = "http://localhost:2000/api/admin/reset-password" // reset password local
+export const urlResetPassword = "https://nodejs-ecommerc.herokuapp.com/api/admin/reset-password"
 
-export const baseHome = "http://localhost:3000" //Home Base
+export const baseHome = "http://localhost:3000" //Home Base client
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
